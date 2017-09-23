aac0200a7cdcc881ee02cdbf02205d58ba27d732
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2017-09-23 10:01:16 +0800

    修改button类，允许button0.x = button1.y写法在脚本中

--- src/main/java/com/whitewoodcity/core/node/Button.java
+++ src/main/java/com/whitewoodcity/core/node/Button.java
@@ -1,22 +1,86 @@
 package com.whitewoodcity.core.node;
 
 import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.scene.Node;
+import javafx.scene.layout.Region;
 
 public class Button extends javafx.scene.control.Button{
-    public DoubleProperty x = new SimpleDoubleProperty();
-    public DoubleProperty y = new SimpleDoubleProperty();
 
-    public Button(String text){
-        super(text);
-        layoutXProperty().bind(x);
-        layoutYProperty().bind(y);
+    public Button() {
+        super();
     }
 
-    public Button(String text, Node graphic){
+    public Button(String text) {
+        super(text);
+    }
+
+    public Button(String text, Node graphic) {
         super(text, graphic);
-        layoutXProperty().bind(x);
-        layoutYProperty().bind(y);
+    }
+
+    public void setX(double x) {
+        setLayoutX(x);
+    }
+
+    public void setY(double y){
+        setLayoutY(y);
+    }
+
+    public double getX(){
+        return getLayoutX();
+    }
+
+    public double getY(){
+
+        return getLayoutY();
+    }
+
+    public DoubleProperty xProperty(){
+        return layoutXProperty();
+    }
+
+    public DoubleProperty yProperty(){
+        return layoutYProperty();
+    }
+
+    public DoubleProperty x_property(){
+        return xProperty();
+    }
+
+    public DoubleProperty y_property(){
+        return yProperty();
+    }
+
+    public DoubleProperty xproperty(){
+        return xProperty();
+    }
+
+    public DoubleProperty yproperty(){
+        return yProperty();
+    }
+
+    public ReadOnlyDoubleProperty width_property(){
+        return widthProperty();
+    }
+
+    public ReadOnlyDoubleProperty height_property(){
+        return heightProperty();
+    }
+
+    public ReadOnlyDoubleProperty widthproperty(){
+        return widthProperty();
+    }
+
+    public ReadOnlyDoubleProperty heightproperty(){
+        return heightProperty();
+    }
+
+    public void setW(double w){
+        setPrefWidth(w);
+    }
+
+    public double getW(){
+        return getWidth();
     }
 }
