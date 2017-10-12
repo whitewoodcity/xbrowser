6abf99d101a16ddec21b519b02a868aaca182300
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2017-10-12 16:40:33 +0800

    x,y chart left

--- src/main/java/com/whitewoodcity/core/node/chart/Chart.java
+++ src/main/java/com/whitewoodcity/core/node/chart/Chart.java
@@ -1,9 +1,13 @@
 package com.whitewoodcity.core.node.chart;
 
 import com.whitewoodcity.core.node.Node;
+import javafx.application.Platform;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.StringProperty;
 import javafx.geometry.Side;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 public abstract class Chart implements Node {
     javafx.scene.chart.Chart body;
@@ -13,83 +17,126 @@ public abstract class Chart implements Node {
     }
 
     public void setX(double x) {
-        body.setLayoutX(x);
+        if(Platform.isFxApplicationThread()) body.setLayoutX(x);
+        else Platform.runLater(()->body.setLayoutX(x));
     }
 
     public void setY(double y){
-        body.setLayoutY(y);
+        if(Platform.isFxApplicationThread()) body.setLayoutY(y);
+        else Platform.runLater(()->body.setLayoutY(y));
     }
 
-    public double getX(){
-        return body.getLayoutX();
+    public double getX() throws InterruptedException,ExecutionException {
+        if(Platform.isFxApplicationThread()) return body.getLayoutX();
+        final FutureTask<Double> task = new FutureTask<>(()->body.getLayoutX());
+        Platform.runLater(task);
+        return task.get();
     }
 
-    public double getY(){
-        return body.getLayoutY();
+    public double getY() throws InterruptedException,ExecutionException{
+        if(Platform.isFxApplicationThread()) return body.getLayoutY();
+        final FutureTask<Double> task = new FutureTask<>(()->body.getLayoutY());
+        Platform.runLater(task);
+        return task.get();
     }
 
-    public DoubleProperty xProperty(){
-        return body.layoutXProperty();
+    public DoubleProperty xProperty() throws InterruptedException,ExecutionException{
+        if(Platform.isFxApplicationThread()) return body.layoutXProperty();
+        final FutureTask<DoubleProperty> task = new FutureTask<>(()->body.layoutXProperty());
+        Platform.runLater(task);
+        return task.get();
     }
 
-    public DoubleProperty yProperty(){
-        return body.layoutYProperty();
+    public DoubleProperty yProperty() throws InterruptedException,ExecutionException{
+        if(Platform.isFxApplicationThread()) return body.layoutYProperty();
+        final FutureTask<DoubleProperty> task = new FutureTask<>(()->body.layoutYProperty());
+        Platform.runLater(task);
+        return task.get();
     }
 
-    public DoubleProperty x_property(){
+    public DoubleProperty x_property()throws InterruptedException,ExecutionException{
         return xProperty();
     }
 
-    public DoubleProperty y_property(){
+    public DoubleProperty y_property()throws InterruptedException,ExecutionException{
         return yProperty();
     }
 
-    public DoubleProperty xproperty(){
+    public DoubleProperty xproperty()throws InterruptedException,ExecutionException{
         return xProperty();
     }
 
-    public DoubleProperty yproperty(){
+    public DoubleProperty yproperty()throws InterruptedException,ExecutionException{
         return yProperty();
     }
 
-    public DoubleProperty widthProperty(){
-        return body.prefWidthProperty();
+
+    public DoubleProperty widthProperty()throws InterruptedException,ExecutionException{
+        if(Platform.isFxApplicationThread()) return body.prefWidthProperty();
+        final FutureTask<DoubleProperty> task = new FutureTask<>(()->body.prefWidthProperty());
+        Platform.runLater(task);
+        return task.get();
     }
 
-    public DoubleProperty heightProperty(){
-        return body.prefHeightProperty();
+    public DoubleProperty heightProperty()throws InterruptedException,ExecutionException{
+        if(Platform.isFxApplicationThread()) return body.prefHeightProperty();
+        final FutureTask<DoubleProperty> task = new FutureTask<>(()->body.prefHeightProperty());
+        Platform.runLater(task);
+        return task.get();
     }
 
-    public DoubleProperty width_property(){
+    public DoubleProperty width_property()throws InterruptedException,ExecutionException{
         return widthProperty();
     }
 
-    public DoubleProperty height_property(){
+    public DoubleProperty height_property()throws InterruptedException,ExecutionException{
         return heightProperty();
     }
 
-    public DoubleProperty widthproperty(){
+    public DoubleProperty widthproperty()throws InterruptedException,ExecutionException{
         return widthProperty();
     }
 
-    public DoubleProperty heightproperty(){
+    public DoubleProperty heightproperty()throws InterruptedException,ExecutionException{
         return heightProperty();
     }
 
     public void setWidth(double width){
-        body.setPrefWidth(width);
+        if(Platform.isFxApplicationThread()) body.setPrefWidth(width);
+        else Platform.runLater(()->body.setPrefWidth(width));
     }
 
-    public double getWidth(){
+    public double getWidth() throws InterruptedException,ExecutionException{
+
+        if(Platform.isFxApplicationThread()){
+            return getWidthBase();
+        }
+        final FutureTask<Double> task = new FutureTask<>(this::getWidthBase);
+        Platform.runLater(task);
+        return task.get();
+
+    }
+
+    private double getWidthBase(){
         if(body.getWidth() > 0.000001) return body.getWidth();
         else return body.getPrefWidth();
     }
 
     public void setHeight(double height){
-        body.setPrefHeight(height);
+        if(Platform.isFxApplicationThread()) body.setPrefHeight(height);
+        else Platform.runLater(()->body.setPrefHeight(height));
     }
 
-    public double getHeight(){
+    public double getHeight() throws InterruptedException,ExecutionException{
+        if(Platform.isFxApplicationThread()){
+            return getHeightBase();
+        }
+        final FutureTask<Double> task = new FutureTask<>(this::getHeightBase);
+        Platform.runLater(task);
+        return task.get();
+    }
+
+    private double getHeightBase(){
         if(body.getHeight() > 0.000001) return body.getHeight();
         else return body.getPrefHeight();
     }
@@ -99,26 +146,42 @@ public abstract class Chart implements Node {
     }
 
     public void setTitle(String title){
-        body.setTitle(title);
+        if(Platform.isFxApplicationThread()) body.setTitle(title);
+        else Platform.runLater(()->body.setTitle(title));
     }
 
-    public String getTitle(){
-        return body.getTitle();
+    public String getTitle() throws InterruptedException, ExecutionException{
+        if(Platform.isFxApplicationThread()){
+            return body.getTitle();
+        }
+        final FutureTask<String> task = new FutureTask<>(()->body.getTitle());
+        Platform.runLater(task);
+        return task.get();
     }
 
-    public StringProperty titleProperty(){
-        return body.titleProperty();
+    public StringProperty titleProperty()throws InterruptedException, ExecutionException{
+        if(Platform.isFxApplicationThread()){
+            return body.titleProperty();
+        }
+        final FutureTask<StringProperty> task = new FutureTask<>(()-> body.titleProperty());
+        Platform.runLater(task);
+        return task.get();
+
     }
 
-    public StringProperty title_property(){
+    public StringProperty title_property()throws InterruptedException, ExecutionException{
         return titleProperty();
     }
 
-    public StringProperty titleproperty(){
+    public StringProperty titleproperty()throws InterruptedException, ExecutionException{
         return titleProperty();
     }
 
     public void setTitleSide(String side){
+        if(Platform.isFxApplicationThread()) setTitleSideBase(side);
+        else Platform.runLater(()->setTitleSideBase(side));
+    }
+    private void setTitleSideBase(String side){
         side = side == null ? "":side;
         side = side.trim().toLowerCase();
         switch (side){
@@ -146,6 +209,11 @@ public abstract class Chart implements Node {
     }
 
     public void setLegendSide(String side){
+        if(Platform.isFxApplicationThread()) setLegendSideBase(side);
+        else Platform.runLater(()->setLegendSideBase(side));
+    }
+
+    private void setLegendSideBase(String side){
         side = side == null ? "":side;
         side = side.trim().toLowerCase();
         switch (side){
--- src/main/java/com/whitewoodcity/core/node/chart/PieChart.java
+++ src/main/java/com/whitewoodcity/core/node/chart/PieChart.java
@@ -1,18 +1,24 @@
 package com.whitewoodcity.core.node.chart;
 
 import io.vertx.core.json.JsonObject;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class PieChart extends Chart{
     public PieChart(){
-        body = new javafx.scene.chart.PieChart();
+        if (Platform.isFxApplicationThread()) body = new javafx.scene.chart.PieChart();
+        else Platform.runLater(() -> body = new javafx.scene.chart.PieChart());
     }
 
     ObservableList<javafx.scene.chart.PieChart.Data> pieChartData =
             FXCollections.observableArrayList();
 
     public void setData(JsonObject data){
+        if(Platform.isFxApplicationThread()) setDataBase(data);
+        else Platform.runLater(()->setDataBase(data));
+    }
+    private void setDataBase(JsonObject data){
 
         for(String key:data.fieldNames()){
             if(data.getValue(key)==null) continue;
@@ -32,6 +38,10 @@ public class PieChart extends Chart{
     }
 
     public void set(String key, double value){
+        if(Platform.isFxApplicationThread()) setBase(key,value);
+        else Platform.runLater(()->setBase(key,value));
+    }
+    private void setBase(String key, double value){
         for(javafx.scene.chart.PieChart.Data datum:pieChartData){
             if(datum.getName().equals(key)){
                 datum.setPieValue(value);
@@ -42,6 +52,10 @@ public class PieChart extends Chart{
     }
 
     public void remove(String key){
+        if(Platform.isFxApplicationThread()) removeBase(key);
+        else Platform.runLater(()->removeBase(key));
+    }
+    private void removeBase(String key){
         javafx.scene.chart.PieChart.Data d=null;
         for(javafx.scene.chart.PieChart.Data datum:pieChartData){
             if(datum.getName().equals(key)){
