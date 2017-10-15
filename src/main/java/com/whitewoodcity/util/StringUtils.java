efb2d512e36516109ee22cd8eb6e72c1137829da
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2017-10-15 21:20:20 +0800

    add replace multiple strings methods
    Java Replacing multiple different substring in a string at once (or in the most efficient way)
    https://stackoverflow.com/questions/1326682/java-replacing-multiple-different-substring-in-a-string-at-once-or-in-the-most

--- src/main/java/com/whitewoodcity/util/StringUtils.java
+++ src/main/java/com/whitewoodcity/util/StringUtils.java
@@ -1,21 +1,20 @@
 package com.whitewoodcity.util;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringUtils {
-    public static boolean isEmpty(String str){
-        return str==null||str.trim().length()==0;
+    public static boolean isEmpty(String str) {
+        return str == null || str.trim().length() == 0;
     }
 
     public static String join(Collection<String> l, String separator) {
         StringBuffer sb = new StringBuffer();
 
-        for(Iterator it = l.iterator(); it.hasNext(); sb.append((String)it.next())) {
+        for (Iterator it = l.iterator(); it.hasNext(); sb.append((String) it.next())) {
             if (sb.length() != 0) {
                 sb.append(separator);
             }
@@ -24,14 +23,14 @@ public class StringUtils {
         return sb.toString();
     }
 
-    public static String replace(String originalString, Map<String, String> tokens){
+    public static String replace(String originalString, Map<String, String> tokens) {
 
-        String patternString = "(" + StringUtil.join(tokens.keySet(), "|") + ")";
+        String patternString = "(" + join(tokens.keySet(), "|") + ")";
         Pattern pattern = Pattern.compile(patternString);
         Matcher matcher = pattern.matcher(originalString);
 
         StringBuffer sb = new StringBuffer();
-        while(matcher.find()) {
+        while (matcher.find()) {
             matcher.appendReplacement(sb, tokens.get(matcher.group(1)));
         }
         matcher.appendTail(sb);
