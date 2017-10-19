4632fc9e91e42219815f464f2701e149167442a6
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2017-10-19 13:28:43 +0800

    change location of imageview

--- src/main/java/com/whitewoodcity/core/bean/XmlV.java
+++ src/main/java/com/whitewoodcity/core/bean/XmlV.java
@@ -92,34 +92,34 @@ public class XmlV {
         this.title = title;
     }
 
-    public List<String> generateResources(){
+    public List<String> generateResources() {
         List<String> resources = new ArrayList<>();
-        if(getPreload()!=null && getPreload().getPreload()!=null &&
-                !getPreload().getPreload().replace("\n","").trim().equals("")){
-            String preload = getPreload().getPreload().replace("\n","").trim();
+        if (getPreload() != null && getPreload().getPreload() != null &&
+                !getPreload().getPreload().replace("\n", "").trim().equals("")) {
+            String preload = getPreload().getPreload().replace("\n", "").trim();
             String[] elements = preload.split(";");
-            for(String element:elements){
+            for (String element : elements) {
                 resources.add(element);
             }
         }
         return resources;
     }
 
-    public Node generateNode(TabContent tabContent) throws Exception{
+    public Node generateNode(TabContent tabContent) throws Exception {
         Json json = getJson();
 
-        String jsonString = json==null?"":json.getJson().trim();
+        String jsonString = json == null ? "" : json.getJson().trim();
 
-        if(jsonString.startsWith("{")) return generateNodeByJsonObject(new JsonObject(jsonString), tabContent);
-        else if(jsonString.startsWith("[")){
+        if (jsonString.startsWith("{")) return generateNodeByJsonObject(new JsonObject(jsonString), tabContent);
+        else if (jsonString.startsWith("[")) {
             JsonObject jsonObject = new JsonObject();
-            jsonObject.put("children",new JsonArray(jsonString));
-            return generateNodeByJsonObject(jsonObject,tabContent);
-        }else{
+            jsonObject.put("children", new JsonArray(jsonString));
+            return generateNodeByJsonObject(jsonObject, tabContent);
+        } else {
             JsonObject jsonObject = new JsonObject();
-            jsonObject.put("type","canvas");
-            jsonObject.put("id","canvas");
-            Canvas node = (Canvas) generateNodeByJsonObject(jsonObject,tabContent);
+            jsonObject.put("type", "canvas");
+            jsonObject.put("id", "canvas");
+            Canvas node = (Canvas) generateNodeByJsonObject(jsonObject, tabContent);
             node.widthProperty().bind(tabContent.getContainer().widthProperty());
             node.heightProperty().bind(tabContent.getContainer().heightProperty());
 
@@ -128,15 +128,22 @@ public class XmlV {
 
     }
 
-    public Node generateNodeByJsonObject(JsonObject jsonObject, TabContent app) throws Exception{
+    public Node generateNodeByJsonObject(JsonObject jsonObject, TabContent app) throws Exception {
         String type = jsonObject.getString("type");
+        if (jsonObject == null) jsonObject = new JsonObject();
         Node node;
 
         JsonArray jsonArray = new JsonArray();
 
-        switch (type==null?"pane":type.toLowerCase()){
+        switch (type == null ? "pane" : type.toLowerCase()) {
             case "canvas":
                 Canvas canvas = new Canvas();
+                Double width = jsonObject.getDouble("width");
+                Double height = jsonObject.getDouble("height");
+                if (width == null) width = 0d;
+                if (height == null) height = 0d;
+                canvas.setWidth(width);
+                canvas.setHeight(height);
                 node = canvas;
                 break;
             case "form":
@@ -152,65 +159,65 @@ public class XmlV {
             case "imageview":
                 ImageView imageView = new ImageView(jsonObject.getString("url"));
                 imageView.setImage(app.getPreload().get(jsonObject.getString("image")));
-                decorateView(imageView,jsonObject);
+                decorateView(imageView, jsonObject);
                 node = imageView;
                 break;
             case "table":
             case "tableview":
                 TableView tableView = new TableView();
                 jsonArray.clear();
-                if(jsonObject.getJsonArray("header")!=null) jsonArray.addAll(jsonObject.getJsonArray("header"));
-                if(jsonObject.getJsonArray("headers")!=null) jsonArray.addAll(jsonObject.getJsonArray("headers"));
-                if(jsonObject.getJsonArray("column")!=null) jsonArray.addAll(jsonObject.getJsonArray("column"));
-                if(jsonObject.getJsonArray("columns")!=null) jsonArray.addAll(jsonObject.getJsonArray("columns"));
+                if (jsonObject.getJsonArray("header") != null) jsonArray.addAll(jsonObject.getJsonArray("header"));
+                if (jsonObject.getJsonArray("headers") != null) jsonArray.addAll(jsonObject.getJsonArray("headers"));
+                if (jsonObject.getJsonArray("column") != null) jsonArray.addAll(jsonObject.getJsonArray("column"));
+                if (jsonObject.getJsonArray("columns") != null) jsonArray.addAll(jsonObject.getJsonArray("columns"));
                 tableView.setHeader(jsonArray);
                 jsonArray.clear();
-                if(jsonObject.getJsonArray("value")!=null) jsonArray.addAll(jsonObject.getJsonArray("value"));
-                if(jsonObject.getJsonArray("values")!=null) jsonArray.addAll(jsonObject.getJsonArray("values"));
+                if (jsonObject.getJsonArray("value") != null) jsonArray.addAll(jsonObject.getJsonArray("value"));
+                if (jsonObject.getJsonArray("values") != null) jsonArray.addAll(jsonObject.getJsonArray("values"));
                 tableView.setValue(jsonArray);
                 tableView.setEditable(jsonObject.getBoolean("editable"));
-                decorateControl(tableView,jsonObject);
+                decorateControl(tableView, jsonObject);
                 node = tableView;
                 break;
             case "textfield":
                 TextField textField = new TextField();
-                decorateControl(textField,jsonObject);
+                decorateControl(textField, jsonObject);
                 textField.setText(jsonObject.getString("text"));
                 node = textField;
                 break;
             case "label":
                 Label label = new Label();
-                decorateControl(label,jsonObject);
+                decorateControl(label, jsonObject);
                 label.setText(jsonObject.getString("text"));
                 node = label;
                 break;
             case "hyperlink":
                 Hyperlink hyperlink = new Hyperlink();
                 hyperlink.setApp(app);
-                decorateControl(hyperlink,jsonObject);
+                decorateControl(hyperlink, jsonObject);
                 hyperlink.setText(jsonObject.getString("text"));
                 hyperlink.setValue(jsonObject.getString("value"));
                 node = hyperlink;
                 break;
             case "button":
                 Button button = new Button();
-                decorateControl(button,jsonObject);
+                decorateControl(button, jsonObject);
                 button.setText(jsonObject.getString("text"));
                 node = button;
                 break;
             case "choicebox":
                 ChoiceBox choiceBox = new ChoiceBox();
-                decorateControl(choiceBox,jsonObject);
+                decorateControl(choiceBox, jsonObject);
                 jsonArray.clear();
-                if(jsonObject.getJsonArray("item")!=null) jsonArray.addAll(jsonObject.getJsonArray("item"));
-                if(jsonObject.getJsonArray("items")!=null) jsonArray.addAll(jsonObject.getJsonArray("items"));
+                if (jsonObject.getJsonArray("item") != null) jsonArray.addAll(jsonObject.getJsonArray("item"));
+                if (jsonObject.getJsonArray("items") != null) jsonArray.addAll(jsonObject.getJsonArray("items"));
                 choiceBox.setItems(jsonArray);
                 choiceBox.setValue(jsonObject.getString("value"));
                 node = choiceBox;
                 break;
             case "piechart":
                 PieChart pieChart = new PieChart();
-                decorateChart(pieChart,jsonObject);
+                decorateChart(pieChart, jsonObject);
                 pieChart.setData(jsonObject.getJsonObject("data"));
                 node = pieChart;
                 break;
@@ -219,31 +226,31 @@ public class XmlV {
             case "scatterchart":
             case "areachart":
                 AxisType xAxisType = AxisType.NUMBER;
-                if(jsonObject.getValue("xaxis")!=null&&
+                if (jsonObject.getValue("xaxis") != null &&
                         jsonObject.getValue("xaxis") instanceof JsonArray &&
-                        jsonObject.getJsonArray("xaxis").size()>0){
-                    if(!(jsonObject.getJsonArray("xaxis").getValue(0) instanceof Number)){
+                        jsonObject.getJsonArray("xaxis").size() > 0) {
+                    if (!(jsonObject.getJsonArray("xaxis").getValue(0) instanceof Number)) {
                         xAxisType = AxisType.CATEGORY;
                     }
                 }
                 AxisType yAxisType = AxisType.NUMBER;
-                if(jsonObject.getValue("data")!=null&&
+                if (jsonObject.getValue("data") != null &&
                         jsonObject.getValue("data") instanceof JsonObject &&
-                        jsonObject.getJsonObject("data").fieldNames().size()>0){
+                        jsonObject.getJsonObject("data").fieldNames().size() > 0) {
                     String key = (String) jsonObject.getJsonObject("data").fieldNames().toArray()[0];
-                    if(jsonObject.getJsonObject("data").getValue(key) instanceof JsonArray &&
-                            jsonObject.getJsonObject("data").getJsonArray(key).size()>0){
-                        if(!(jsonObject.getJsonObject("data").getJsonArray(key).getValue(0) instanceof Number)
-                                && !(jsonObject.getJsonObject("data").getJsonArray(key).getValue(0) instanceof JsonObject)){
+                    if (jsonObject.getJsonObject("data").getValue(key) instanceof JsonArray &&
+                            jsonObject.getJsonObject("data").getJsonArray(key).size() > 0) {
+                        if (!(jsonObject.getJsonObject("data").getJsonArray(key).getValue(0) instanceof Number)
+                                && !(jsonObject.getJsonObject("data").getJsonArray(key).getValue(0) instanceof JsonObject)) {
                             yAxisType = AxisType.CATEGORY;
-                        }else if((jsonObject.getJsonObject("data").getJsonArray(key).getValue(0) instanceof JsonObject &&
-                                !(jsonObject.getJsonObject("data").getJsonArray(key).getJsonObject(0).getValue("y") instanceof Number))){
+                        } else if ((jsonObject.getJsonObject("data").getJsonArray(key).getValue(0) instanceof JsonObject &&
+                                !(jsonObject.getJsonObject("data").getJsonArray(key).getJsonObject(0).getValue("y") instanceof Number))) {
                             yAxisType = AxisType.CATEGORY;
                         }
                     }
                 }
                 XYChart xyChart = new XYChart(type, xAxisType, yAxisType);
-                decorateChart(xyChart,jsonObject);
+                decorateChart(xyChart, jsonObject);
                 xyChart.setXScale(jsonObject.getJsonArray("xaxis"));
                 xyChart.setXLabel(jsonObject.getString("xlabel"));
                 xyChart.setYLabel(jsonObject.getString("ylabel"));
@@ -252,7 +259,7 @@ public class XmlV {
                 break;
             case "bubblechart":
                 XYChart bubbleChart = new XYChart(type, AxisType.NUMBER, AxisType.NUMBER);
-                decorateChart(bubbleChart,jsonObject);
+                decorateChart(bubbleChart, jsonObject);
                 bubbleChart.setXLabel(jsonObject.getString("xlabel"));
                 bubbleChart.setYLabel(jsonObject.getString("ylabel"));
                 bubbleChart.setData(jsonObject.getJsonObject("data"));
@@ -261,17 +268,17 @@ public class XmlV {
             default:
                 Pane pane = new Pane();
                 jsonArray = jsonObject.getJsonArray("children");
-                if(jsonArray!=null){
-                    for(Object jo:jsonArray){
-                        Node child = generateNodeByJsonObject((JsonObject)jo, app);
-                        if(child.getNode()!=null) pane.add(child);
+                if (jsonArray != null) {
+                    for (Object jo : jsonArray) {
+                        Node child = generateNodeByJsonObject((JsonObject) jo, app);
+                        if (child.getNode() != null) pane.add(child);
                     }
                 }
                 node = pane;
                 break;
         }
 
-        if(jsonObject.getString("id")!=null){
+        if (jsonObject.getString("id") != null) {
             node.setId(jsonObject.getString("id"));
             app.getContext().put(jsonObject.getString("id"), node);
         }
@@ -279,42 +286,42 @@ public class XmlV {
         return node;
     }
 
-    public void decorateView(View node, JsonObject jsonObject){
+    public void decorateView(View node, JsonObject jsonObject) {
         Double x, y, width, height;
         x = jsonObject.getDouble("x");
         y = jsonObject.getDouble("y");
         width = jsonObject.getDouble("width");
         height = jsonObject.getDouble("height");
-        node.setX(x==null?0:x);
-        node.setY(y==null?0:y);
-        if(width!=null) node.setWidth(width);
-        if(height!=null) node.setHeight(height);
+        node.setX(x == null ? 0 : x);
+        node.setY(y == null ? 0 : y);
+        if (width != null) node.setWidth(width);
+        if (height != null) node.setHeight(height);
     }
 
 
-    public void decorateControl(Control node, JsonObject jsonObject){
+    public void decorateControl(Control node, JsonObject jsonObject) {
         Double x, y, width, height;
         x = jsonObject.getDouble("x");
         y = jsonObject.getDouble("y");
         width = jsonObject.getDouble("width");
         height = jsonObject.getDouble("height");
-        node.setX(x==null?0:x);
-        node.setY(y==null?0:y);
-        if(width!=null) node.setWidth(width);
-        if(height!=null) node.setHeight(height);
+        node.setX(x == null ? 0 : x);
+        node.setY(y == null ? 0 : y);
+        if (width != null) node.setWidth(width);
+        if (height != null) node.setHeight(height);
         node.setName(jsonObject.getString("name"));
     }
 
-    public void decorateChart(Chart node, JsonObject jsonObject){
+    public void decorateChart(Chart node, JsonObject jsonObject) {
         Double x, y, width, height;
         x = jsonObject.getDouble("x");
         y = jsonObject.getDouble("y");
         width = jsonObject.getDouble("width");
         height = jsonObject.getDouble("height");
-        node.setX(x==null?0:x);
-        node.setY(y==null?0:y);
-        if(width!=null) node.setWidth(width);
-        if(height!=null) node.setHeight(height);
+        node.setX(x == null ? 0 : x);
+        node.setY(y == null ? 0 : y);
+        if (width != null) node.setWidth(width);
+        if (height != null) node.setHeight(height);
         node.setTitle(jsonObject.getString("title"));
         node.setTitleSide(jsonObject.getString("titleside"));
         node.setLegendSide(jsonObject.getString("legendside"));
