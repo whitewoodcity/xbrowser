0419a52a762586141c671ca9e5ef7c0f7b4aad3e
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2017-11-03 10:35:22 +0800

    adjust start browser code to BrowserStage class

--- src/main/java/com/whitewoodcity/Main.java
+++ src/main/java/com/whitewoodcity/Main.java
@@ -1,8 +1,8 @@
 package com.whitewoodcity;
 
+import com.whitewoodcity.core.node.BrowserStage;
 import com.whitewoodcity.security.ApplicationSecurityManager;
 import com.whitewoodcity.thread.CustomerThread;
-import com.whitewoodcity.ui.PagePane;
 import com.whitewoodcity.util.Res;
 import com.whitewoodcity.verticle.DatagramVerticle;
 import com.whitewoodcity.verticle.WebClientVerticle;
@@ -13,12 +13,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import javax.script.ScriptEngineManager;
@@ -39,59 +34,44 @@ public class Main extends Application {
     public static Map<String, StringProperty> namesMap = new HashMap<>();
     public static Map<String, Buffer> bufferMap = new HashMap<>();
 
-    private PagePane tabPane;
+    private BrowserStage browser;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setOnCloseRequest(e -> {
-            try {
-                this.stop();
-            } catch (Exception e1) {
-                Platform.exit();
-                System.exit(0);
-            }
-        });
+        displayBrowser();
+//        primaryStage.show();
+//        primaryStage.setOnCloseRequest(e ->stop());
+    }
 
-        primaryStage.titleProperty().bind(namesMap.get("title"));
-        primaryStage.getIcons().add(new Image("icons/xbrowser.png"));
-        Group root = new Group();
-        Button button = new Button("+");
-        tabPane = new PagePane();
+    public void displayBrowser(){
+        if(browser==null){
+            browser = new BrowserStage();
 
-        Scene scene = new Scene(root, 960, 540);
-        root.getChildren().addAll(tabPane,button);
-        primaryStage.setScene(scene);
-//        scene.getStylesheets().add(ClassLoader.getSystemResource("css/main.css").toExternalForm());
-        primaryStage.show();
-        primaryStage.setResizable(true);
+            browser.setOnCloseRequest(e -> {
+                browser.close();
+                stop();
+            });
 
-        tabPane.prefWidthProperty().bind(scene.widthProperty());
-        tabPane.prefHeightProperty().bind(scene.heightProperty());
-        tabPane.setTabMaxWidth(100);
-        tabPane.setTabMinWidth(100);
+            browser.titleProperty().unbind();
+            browser.titleProperty().bind(namesMap.get("title"));
+            browser.getIcons().add(new Image("icons/xbrowser.png"));
+        }else{
+            browser.display();
+        }
+    }
 
-        tabPane.buildPane();
-
-        tabPane.prefWidthProperty().bind(scene.widthProperty());
-        StackPane headerArea = (StackPane) tabPane.lookup(".tab-header-area");
-//        StackPane backPane= (StackPane) tabPane.lookup(".tab-header-background");
-//        backPane.setStyle("-fx-background-color:#F4F4F4");
-        Insets insets = headerArea.getInsets();
-        insets = new Insets(insets.getTop(),insets.getRight(),insets.getBottom(),insets.getLeft()+button.getWidth());
-        headerArea.setPadding(insets);
-
-        button.setLayoutX(insets.getLeft()-button.getWidth());
-        button.setLayoutY(insets.getTop());
-        button.setOnAction(event -> {
-            tabPane.buildPane();
-        });
+    public void closeBrowser(){
+        if(browser!=null) browser.close();
     }
 
     @Override
-    public void stop() throws Exception {
+    public void stop(){
+        closeBrowser();
+        exit();
+    }
+
+    private void exit(){
         vertx.close();
-        tabPane.close();
-        super.stop();
         Platform.exit();
         System.exit(0);
     }
---
+++ src/main/java/com/whitewoodcity/core/node/BrowserStage.java
@@ -0,0 +1,59 @@
+package com.whitewoodcity.core.node;
+
+import com.whitewoodcity.ui.PagePane;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class BrowserStage extends Stage{
+    Group root = new Group();
+    Button button = new Button("+");
+    PagePane tabPane = new PagePane();
+
+    Scene scene;
+
+    public BrowserStage() {
+        scene = new Scene(root, 960, 540);
+        root.getChildren().addAll(tabPane,button);
+
+        tabPane.prefWidthProperty().bind(scene.widthProperty());
+        tabPane.prefHeightProperty().bind(scene.heightProperty());
+        tabPane.setTabMaxWidth(100);
+        tabPane.setTabMinWidth(100);
+
+//        tabPane.buildPane();
+
+        this.setScene(scene);
+
+        display();
+
+        tabPane.prefWidthProperty().bind(scene.widthProperty());
+        StackPane headerArea = (StackPane) tabPane.lookup(".tab-header-area");
+//        StackPane backPane= (StackPane) tabPane.lookup(".tab-header-background");
+//        backPane.setStyle("-fx-background-color:#F4F4F4");
+        Insets insets = headerArea.getInsets();
+        insets = new Insets(insets.getTop(),insets.getRight(),insets.getBottom(),insets.getLeft()+button.getWidth());
+        headerArea.setPadding(insets);
+
+        button.setLayoutX(insets.getLeft()-button.getWidth());
+        button.setLayoutY(insets.getTop());
+        button.setOnAction(event -> {
+            tabPane.buildPane();
+        });
+
+        this.setResizable(true);
+    }
+
+    public void display(){
+        super.show();
+        tabPane.buildPane();
+    }
+
+    public void close(){
+        super.close();
+        tabPane.close();
+    }
+}
